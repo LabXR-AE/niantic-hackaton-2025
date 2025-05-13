c9f05690c50e6d26587f4834cac19f274b37eb8f
Fernando De Lucchi <69133647+fdelucchijr@users.noreply.github.com>
2025-05-13 00:47:49 -0500

    .

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,6 +1,6 @@
 export const headerData = {
   links: [],
-  actions: [{ text: 'Inscríbete', href: 'https://google.com', target: '_blank' }],
+  actions: [{ text: 'Inscríbete', href: 'https://forms.gle/z2dhtyZT6Pfk3va87', target: '_blank' }],
 };
 
 export const footerData = {
@@ -9,10 +9,10 @@ export const footerData = {
 
   }],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://google.com' },
+    // { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/LabXR-AE' },
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
